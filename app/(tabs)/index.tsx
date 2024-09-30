d60f7258fff429a85022feb922edd4be263f8ffc
Jason Bell <jasonbell143@gmail.com>
2024-09-30 15:39:50 +0100

    added marker for user location

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,26 @@
 import { Platform, Text, View } from "react-native";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      console.log(location);
+    })();
+  }, []);
+
   if (Platform.OS === "android" || Platform.OS === "ios") {
     return (
       <View
@@ -12,6 +31,13 @@ export default function Index() {
         }}
       >
         <MapView
+          showsUserLocation={true}
+          onPress={(e) => {
+            Location.getCurrentPositionAsync({}).then((location) => {
+              setLocation(location);
+              console.log("Current location:", location);
+            });
+          }}
           style={{
             width: "100%",
             height: "100%",
