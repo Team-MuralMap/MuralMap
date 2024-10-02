5ed0511539fab3f927cc85d01fd78ae6dd0f0d1d
Jason Bell <jasonbell143@gmail.com>
2024-10-02 11:42:30 +0100

    added site markers and default image preview

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
-import { Platform, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import {
+  Dimensions,
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
+import { fetchSites } from "@/client/client.mjs";
 
 export default function Index() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState<null | Location.LocationObject>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<null | string>(null);
 
   useEffect(() => {
     (async () => {
@@ -17,9 +27,26 @@ export default function Index() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-      console.log(location);
     })();
   }, []);
+
+  const [sites, setSites] = useState([]);
+  const [isSitesLoading, setIsSitesLoading] = useState(true);
+
+  useEffect(() => {
+    setIsSitesLoading(true);
+    fetchSites().then(({ sites }) => {
+      setSites(sites);
+      setIsSitesLoading(false);
+    });
+  });
+
+  const [previewedSite, setPreviewedSite] = useState<null | number>(null);
+
+  function getSitePreviewURL(site_id: number) {
+    site_id;
+    return "https://archive.nuartfestival.no/img/a_102_13020_713.jpg";
+  }
 
   if (Platform.OS === "android" || Platform.OS === "ios") {
     return (
@@ -38,11 +65,24 @@ export default function Index() {
               console.log("Current location:", location);
             });
           }}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        />
+          style={styles.map}
+        >
+          {sites.map(({ latitude, longitude, site_id }) => (
+            <Marker key={site_id} coordinate={{ latitude, longitude }}>
+              <Callout
+                onPress={() => console.log(`You just pressed site ${site_id}!`)}
+              >
+                <Text>
+                  <Image
+                    src={getSitePreviewURL(site_id)}
+                    style={styles.sitePreviewImg}
+                  />
+                </Text>
+              </Callout>
+            </Marker>
+          ))}
+        </MapView>
+        {previewedSite ? <Text>{previewedSite}</Text> : null}
       </View>
     );
   } else {
@@ -53,3 +93,21 @@ export default function Index() {
     );
   }
 }
+
+const screenWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  sitePreviewImg: {
+    width: screenWidth / 3,
+    height: screenWidth / 3,
+    borderColor: "#990000",
+    borderWidth: 20,
+    borderStyle: "solid",
+    paddingTop: 10,
+    backgroundColor: "#00ff00",
+  },
+});
