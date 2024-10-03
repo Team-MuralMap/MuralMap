9f598365b846594eec18aef99b599f98d5733491
Jason Bell <jasonbell143@gmail.com>
2024-10-03 14:18:04 +0100

    added a user context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
+import { UserProvider } from "@/contexts/UserContext";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-    </Stack>
+    <UserProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </UserProvider>
   );
 }
